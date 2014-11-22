2118cd217c31d78d3d1380c22dc5de4cea6180c9
Anthony FERTIL <anthony.fertil@niji.fr>
2014-11-22 14:16:51 +0100

    update show model

--- app/models/show.server.model.js
+++ app/models/show.server.model.js
@@ -20,10 +20,27 @@ var ShowSchema = new Schema({
 		type: Date,
 		default: Date.now
 	},
-	user: {
-		type: Schema.ObjectId,
+	airsDayOfWeek: String,
+	airsTime: String,
+	firstAired: Date,
+	genre: [String],
+	network: String,
+	overview: String,
+	rating: Number,
+	ratingCount: Number,
+	status: String,
+	poster: String,
+	subscribers: [{
+		type: mongoose.Schema.Types.ObjectId,
 		ref: 'User'
-	}
+	}],
+	episodes: [{
+		season: Number,
+		episodeNumber: Number,
+		episodeName: String,
+		firstAired: Date,
+		overview: String
+	}]
 });
 
 mongoose.model('Show', ShowSchema);
